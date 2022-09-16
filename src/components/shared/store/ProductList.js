dfcb4c81a5ff30382058861633c3931cc195c6d9
Juan Cruz <jumacruz01@gmail.com>
2022-09-16 17:40:48 -0600

    api started

--- src/components/shared/store/ProductList.js
+++ src/components/shared/store/ProductList.js
@@ -1,10 +1,19 @@
 import Products from "./Products";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ContextProducts } from "../../../services/MemoryProducts";
 import { Outlet } from "react-router";
+import { OrderProducts } from "../../../services/OrderProducts";
 
 function ProductsList() {
-    const [state] = useContext(ContextProducts);
+
+    const [state, dispatch] = useContext(ContextProducts);
+
+    useEffect(() => {
+        const items = OrderProducts();
+        dispatch({ type: 'place', items });
+    },[]);
+
+    
     return ( 
         <>
             {state.order.map(id=> <Products key={id} {...state.objects[id]}></Products>)}
--- src/services/MemoryProducts.js
+++ src/services/MemoryProducts.js
@@ -1,6 +1,6 @@
 import { createContext, useReducer } from "react";
-import landscape from '../components/assets/img/landscape.jpg';
-
+// import landscape from '../components/assets/img/landscape.jpg';
+// import landscape from '../../public/products.json';
 // const MetaProductsList = [
 //     {
 //         "id":"1",
@@ -37,18 +37,18 @@ import landscape from '../components/assets/img/landscape.jpg';
 //     }
 // ];
 
-// const InitialState = {
-//     order: [],
-//     objects: {}
-// };
+const InitialState = {
+    order: [],
+    objects: {}
+};
 
-const memory = localStorage.getItem('items')
-const InitialState = memory
-    ? JSON.parse(memory)
-    : {
-        order: [],
-        objects: {}
-    };
+// const memory = localStorage.getItem('items')
+// const InitialState = 
+//     memory ? JSON.parse(memory)
+//     : {
+//         order: [],
+//         objects: {}
+//     };
 
 function reductor(state, action) {
     switch (action.type) {
@@ -58,7 +58,7 @@ function reductor(state, action) {
                 order: items.map(item => item.id),
                 objects: items.reduce((object, item) => ({...object, [item.id]: item}), {})
             };
-            localStorage.setItem('items', JSON.stringify(newState))
+            // localStorage.setItem('items', JSON.stringify(newState))
             return newState;
         };
         case 'create': {
@@ -70,7 +70,7 @@ function reductor(state, action) {
                     [id]: action.item
                 }
             };
-            localStorage.setItem('items', JSON.stringify(newState))
+            // localStorage.setItem('items', JSON.stringify(newState))
             return newState;
         };
         case 'update': {
@@ -80,7 +80,7 @@ function reductor(state, action) {
                 ...action.item
             };
             const newState = { ...state };
-            localStorage.setItem('items', JSON.stringify(newState))
+            // localStorage.setItem('items', JSON.stringify(newState))
             return newState;
         };
         case 'erase': {
@@ -91,7 +91,7 @@ function reductor(state, action) {
                 order: newOrder,
                 objects: state.objects
             };
-            localStorage.setItem('items', JSON.stringify(newState))
+            // localStorage.setItem('items', JSON.stringify(newState))
             return newState;
         };
         default:
