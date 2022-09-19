10b43db0eb2d89906367b0b1a630931a2744f46e
Juan Cruz <jumacruz01@gmail.com>
2022-09-19 10:57:27 -0600

    stable version

--- src/components/shared/store/ProductList.js
+++ src/components/shared/store/ProductList.js
@@ -1,19 +1,12 @@
 import Products from "./Products";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { ContextProducts } from "../../../services/MemoryProducts";
 import { Outlet } from "react-router";
-import { OrderProducts } from "../../../services/OrderProducts";
 
 function ProductsList() {
 
-    const [state, dispatch] = useContext(ContextProducts);
+    const [state] = useContext(ContextProducts);
 
-    useEffect(() => {
-        const items = OrderProducts();
-        dispatch({ type: 'place', items });
-    },[]);
-
-    
     return ( 
         <>
             {state.order.map(id=> <Products key={id} {...state.objects[id]}></Products>)}
